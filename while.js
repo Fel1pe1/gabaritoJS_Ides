88f2cea23e264ead480191dac5edd7ae8df8832d
Aluno LAB <aluno.lab@ides.org.br>
2025-12-03 14:11:48 -0300

    atualização

---
+++ while.js
@@ -0,0 +1,24 @@
+//while
+
+/* let opcaoUsuario = 1 */
+let numeroUsuario = 0;
+
+while (numeroUsuario <= 100) {
+    alert(`seu numero é: ${numeroUsuario}`);
+    numeroUsuario += 5;   
+}
+
+/* while (opcaoUsuario != 2) {
+
+    opcaoUsuario = parseInt(prompt(`deseja encerrar \n -1 nao \n -2 sim`))
+} */
+
+/* while (opcaoUsuario != 1){
+    opcaoUsuario ++
+    alert("foi")
+} */
+
+/* do {
+    opcaoUsuario = parseInt(prompt("Deseja encerrar? \n1-sim \n 2-nao")
+    )
+}while (opcaoUsuario = 0) */
